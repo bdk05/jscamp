24b725250052f4b1a10df2c20eec1add7cc14637
Berna KASALAK <bdemir05@gmail.com>
2021-10-19 14:32:18 +0300

    18 Ekim ders Sonu

---
+++ src/app.js
@@ -0,0 +1,31 @@
+console.log("Merhaba Kodlama io")
+//js type safe değildir
+//dolarDun="Ankara"
+let dolarBugun=9.30
+let  dolarDun=9.20
+
+{
+let dolarDun=9.10
+
+}
+
+
+console.log(dolarDun)
+
+const euroDun=11.2 //const ile verilen değer artık değişmez
+//euroDun=11
+
+console.log(euroDun)
+//array dizi
+//camelCasing
+//PascalCasing
+let konutKredileri=["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+console.log("<ul>")
+
+for(let i=0;i<konutKredileri.length;i++){
+    console.log("<li>" +konutKredileri[i]+ "</li>")
+}
+
+console.log("</ul>")
+
